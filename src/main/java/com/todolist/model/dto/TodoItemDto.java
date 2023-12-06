f51df4cc25a2c08f491ae03042858b18aa816f5b
MacPro <macpro@MacPros-MBP.fritz.box>
2023-12-06 07:43:11 +0100

    Adding Schema in the TodoItemDto

--- src/main/java/com/todolist/model/dto/TodoItemDto.java
+++ src/main/java/com/todolist/model/dto/TodoItemDto.java
@@ -2,6 +2,8 @@ package com.todolist.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.todolist.model.entity.TodoStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
@@ -9,17 +11,23 @@ import java.time.LocalDateTime;
 
 @Builder
 public record TodoItemDto(
+        @Schema(description = "The unique identifier of the todo item")
         @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         Long id,
+        @Schema(description = "The description of the todo item")
         @NotEmpty
         String description,
+        @Schema(description = "The status of the todo item", implementation = TodoStatus.class)
         @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         String status,
+        @Schema(description = "The creation date")
         @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime creationDateTime,
+        @Schema(description = "The due date")
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime dueDateTime,
+        @Schema(description = "The done date")
         @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime doneDateTime
